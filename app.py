35406c3edbe00b5a01de6884063ce17f76eaa689
RiskaFajriati <riska.fj@mhs.mdp.ac.id>
2024-01-10 18:46:14 +0700

    hehe

---
+++ app.py
@@ -0,0 +1,376 @@
+from tensorflow.keras.models import load_model
+import numpy as np
+from flask_limiter import Limiter, RateLimitExceeded
+from flask_limiter.util import get_remote_address
+from tensorflow.keras.preprocessing.image import load_img
+from tensorflow.keras.applications.resnet50 import preprocess_input
+from tensorflow.keras.preprocessing import image
+from functools import wraps
+from flask import Flask, jsonify, request
+import pymysql
+import io
+from PIL import Image
+from dotenv import load_dotenv
+from datetime import datetime, timedelta
+from werkzeug.security import generate_password_hash, check_password_hash
+import os
+from marshmallow import Schema, fields, validate
+import jwt
+
+# #################### ENV #########################
+load_dotenv()
+
+app = Flask(__name__)
+model = load_model('songket_brand_clf_resnet50.h5')
+app.config['MYSQL_HOST'] = os.getenv("DB_HOST")
+app.config['MYSQL_USER'] = os.getenv("DB_USER")
+app.config['MYSQL_PASSWORD'] = os.getenv("DB_PASSWORD")
+app.config['MYSQL_DB'] = os.getenv("DB_NAME")
+# app.config['IMAGE_UPLOADS'] = "C:\\Users\\user\\Documents\\GitHub\\songketTA"
+app.config['IMAGE_UPLOADS'] = "/home/azureuser/songketTA"
+app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
+mysql = pymysql.connect(
+    host=app.config['MYSQL_HOST'],
+    user=app.config['MYSQL_USER'],
+    password=app.config['MYSQL_PASSWORD'],
+    db=app.config['MYSQL_DB'],
+    ssl_ca='DigiCertGlobalRootCA.crt.pem'
+)
+
+def get_user_id():
+    headers = request.headers['Authorization']
+    token = headers.split()[1]
+    data = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
+    return str(data['id'])
+
+
+# Limiter Instance
+limiter = Limiter(
+    get_remote_address,
+    app=app,
+    default_limits=["200 per day", "50 per hour"],
+    storage_uri="memory://"
+)
+
+# Validator Class
+
+class LoginSchema(Schema):
+    user = fields.String(
+        required=True,
+        validate=validate.Length(4),
+        error_messages={
+            "required": "Username harus diisi",
+            "validator_failed": "Username minimal 4 huruf"
+        }
+    )
+    password = fields.String(
+        required=True,
+        validate=validate.Length(8),
+        error_messages={
+            "required": "Password harus diisi",
+            "validator_failed": "Password minimal 8 huruf"
+        }
+    )
+
+
+
+
+##################### JWT #########################
+
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+        # jwt is passed in the request header
+        if 'Authorization' in request.headers:
+            headers = request.headers['Authorization']
+            token = headers.split()[1]
+        # return 401 if token is not passed
+        if not token:
+            return jsonify({'message': 'Token tidak ditemukan !!'}), 401
+        try:
+            # decoding the payload to fetch the stored details
+            data = jwt.decode(
+                token, app.config['SECRET_KEY'], algorithms=['HS256'])
+            current_user = getUsersbyId(data['id'])
+        except Exception as e:
+            print(e)
+            return jsonify({
+                'message': 'Token tidak valid !!'
+            }), 401
+        # returns the current logged in users context to the routes
+        return f(current_user, *args, **kwargs)
+
+    return decorated
+
+
+##################### Login #########################
+@app.route('/login', methods=['POST'])
+def login():
+
+    try:
+        data = request.get_json()
+        login_schema = LoginSchema()
+        errors = login_schema.validate(data)
+        if errors:
+            return jsonify({"error": errors}), 422
+        user = getUsersbyUsername(data["user"])
+        if not user:
+            # returns 401 if user does not exist
+            return jsonify({
+                'message': 'Akun tidak ada !!'
+            }), 400
+
+        if check_password_hash(user['password'], data["password"]):
+            # generates the JWT Token
+            print("HOHOHOHOHO")
+            token = jwt.encode({
+                'id': user['id'],
+                'exp': datetime.utcnow() + timedelta(minutes=131400)
+            }, app.config['SECRET_KEY'])
+            encoded_jwt = token.decode('UTF-8')
+            print(type(token))
+            return jsonify({"token": encoded_jwt,
+                            "status": user['status']}), 200
+        # returns 403 if password is wrong
+        return jsonify({"message": "Akun tidak bisa di verifikasi!"}), 403
+    except Exception as e:
+        return jsonify({"message": e}), 400
+
+
+##################### Register #########################
+@app.route('/register', methods=['POST'])
+def signup():
+    try:
+        users = request.json['user']
+        status = request.json['status']
+        password = request.json['password']
+        # checking for existing user
+        user = getUsersbyUsername(users)
+        if not user:
+            cur = mysql.cursor()
+            cur.execute(
+                '''INSERT INTO users (user, password, status) VALUES (%s, %s, %s)''', (users, generate_password_hash(password), status))
+            mysql.commit()
+            cur.close()
+            return jsonify({"message": "Berhasil Didaftarkan"}), 201
+        else:
+            return jsonify({"message": "Username sudah ada."}), 400
+    except Exception as e:
+        return jsonify({"message": e}), 400
+
+
+######################## Do SOmethink here ############################
+@app.route("/upload", methods=["POST"])
+@token_required
+def upload_image(current_user):
+    if current_user["status"] == 0:
+        try:
+            with limiter.limit("50 per day", key_func=get_user_id):
+                if request.files:
+                    # Check Image Mimetype
+                    file = request.files["image"]
+                    if (file.mimetype != 'image/jpeg' and file.mimetype != 'image/png') :
+                        return jsonify({"message": "Gunakan Format jpeg atau png!"}), 400
+                    print(file.mimetype)
+                    # Read Image
+                    # image = file.read()
+                    # image = Image.open(io.BytesIO(image))
+                    file_path = os.path.join(app.config["IMAGE_UPLOADS"], file.filename)
+                    file.save(file_path)
+                    img = load_img(file_path, target_size=(224, 224))
+                    x = image.img_to_array(img)
+                    x = np.expand_dims(x, axis=0)
+                    x = preprocess_input(x)
+                    class_prediction=model.predict(x) 
+                    classes_x=np.argmax(class_prediction,axis=1)
+                    kelas = ''
+                    if classes_x.item() == 0 :
+                        kelas = 'Bintang Berante'
+                    elif classes_x.item() == 1 :
+                        kelas = 'Nago Besaung'
+                    elif classes_x.item() == 2 :
+                        kelas = 'Nampan Perak'
+                    elif classes_x.item() == 3 :
+                        kelas = 'Pulir'
+                    else :
+                        kelas = 'Jantung'
+                    # Do model prediction
+                    # delete photo
+                    os.remove(file_path)
+                    # Return response
+                    return jsonify({
+                        "message": "some-prediction value",
+                        "data": kelas
+                        }), 200
+                else:
+                    return jsonify({"message": "Somethink went wronk"}), 400
+        except RateLimitExceeded:
+            data = {"message": "Limit Exceed"}
+            return jsonify(data), 429
+    else:
+        try:
+            with limiter.limit("75 per day", key_func=get_user_id):
+                if request.files:
+                    # Check Image Mimetype
+                    file = request.files["image"]
+                    if (file.mimetype != 'image/jpeg' and file.mimetype != 'image/png') :
+                        return jsonify({"message": "Gunakan Format jpeg atau png!"}), 400
+                    print(file.mimetype)
+                    # Read Image
+                    # image = file.read()
+                    # image = Image.open(io.BytesIO(image))
+                    file_path = os.path.join(app.config["IMAGE_UPLOADS"], file.filename)
+                    file.save(file_path)
+                    img = load_img(file_path, target_size=(224, 224))
+                    x = image.img_to_array(img)
+                    x = np.expand_dims(x, axis=0)
+                    x = preprocess_input(x)
+                    class_prediction=model.predict(x) 
+                    classes_x=np.argmax(class_prediction,axis=1)
+                    kelas = ''
+                    if classes_x.item() == 0 :
+                        kelas = 'Bintang Berante'
+                    elif classes_x.item() == 1 :
+                        kelas = 'Nago Besaung'
+                    elif classes_x.item() == 2 :
+                        kelas = 'Nampan Perak'
+                    elif classes_x.item() == 3 :
+                        kelas = 'Pulir'
+                    else :
+                        kelas = 'Jantung'
+                    # Do model prediction
+                    # delete photo
+                    os.remove(file_path)
+                    # Return response
+                    return jsonify({
+                        "message": "some-prediction value",
+                        "data": kelas
+                        }), 200
+                else:
+                    return jsonify({"message": "Somethink went wronk"}), 400
+        except RateLimitExceeded:
+            data = {"message": "Limit Exceed"}
+            return jsonify(data), 429
+        
+
+######################################################################
+
+
+##################### Hello Wolrd #########################
+@app.route('/', methods=['GET'])
+@token_required
+def helloworld(current_user):
+    if (request.method == 'GET'):
+        data = {"data": "Hello World"}
+        return jsonify(data)
+
+@app.route('/check', methods=['GET'])
+@token_required
+def check_limit(current_user):
+    if current_user["status"] == 0:
+        try:
+            with limiter.limit("50 per day", key_func=get_user_id):
+                data = {"data": "Hello Misqueen"}
+                return jsonify(data)
+        except RateLimitExceeded:
+            data = {"message": "Limit Exceed"}
+            return jsonify(data), 429
+    else:
+        try:
+            with limiter.limit("75 per day", key_func=get_user_id):
+                data = {"data": "Hello Sultan"}
+                return jsonify(data)
+        except RateLimitExceeded:
+            data = {"message": "Limit Exceed"}
+            return jsonify(data), 429
+            
+    
+##################### All users #########################
+@app.route('/users', methods=['GET'])
+@token_required
+def getUsers(current_user):
+    try:
+        items = []
+        cur = mysql.cursor()
+        cur.execute('''SELECT * FROM users''')
+        data = cur.fetchall()
+        cur.close()
+        for item in data:
+            item = {
+                'id': item[0],
+                'user': item[1],
+                'password': item[2],
+                'status': item[3],
+            }
+            items.append(item)
+        return jsonify(items), 200
+    except Exception as e:
+        item = {
+            'status': False,
+            'message': f"{e}",
+        }
+        items.append(item)
+        return jsonify(items), 500
+
+
+##################### User By Id #########################
+@app.route('/users/<int:id>', methods=['GET'])
+def getUserById(id):
+    try:
+        response = getUsersbyId(id)
+        return jsonify(response), 200
+    except Exception as e:
+        response = {
+            'status': False,
+            'message': f"{e}",
+        }
+        return jsonify(response), 500
+
+################### Handle Not Found #####################
+
+
+@app.errorhandler(404)
+def not_found_error(e):
+    return jsonify({'message': 'Not Found'}), 404
+
+################## Custom Function #####################
+
+
+def getUsersbyId(params):
+    cur = mysql.cursor()
+    cur.execute('''SELECT * FROM users WHERE id = %s''', (params,))
+    data = cur.fetchall()
+    cur.close()
+    if not data:
+        response = None
+    else:
+        response = {
+            'id': data[0][0],
+            'user': data[0][1],
+            'password': data[0][2],
+            'status': data[0][3],
+        }
+    return response
+
+
+def getUsersbyUsername(params):
+    cur = mysql.cursor()
+    cur.execute('''SELECT * FROM users WHERE user = %s''', (params,))
+    data = cur.fetchall()
+    cur.close()
+    if not data:
+        response = None
+    else:
+        response = {
+            'id': data[0][0],
+            'user': data[0][1],
+            'password': data[0][2],
+            'status': data[0][3],
+        }
+    return response
+
+
+if __name__ == '__main__':
+    app.run()
